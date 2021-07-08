ce8d6aaa968ef3f8869eacecf75b4ff777678d2f
Mark Cianciosa <cianciosamr@ornl.gov>
2021-07-08 11:11:28 -0400

    Add scablacs library when mkl is used.

--- cmake/FindSCALAPACK.cmake
+++ cmake/FindSCALAPACK.cmake
@@ -10,6 +10,11 @@ Provides the following variables:
 find_library (SCALAPACK_LIBRARY
               NAMES scalapack scalapack-openmpi mkl_scalapack_ilp64
               DOC "scalapack library")
+
+if (${SCALAPACK_LIBRARY} STREQUAL mkl_scalapack_ilp64)
+    find_library (SCABLACS_LIBRARY mkl_blacs_intelmpi_lp64)
+endif ()
+
 mark_as_advanced (SCALAPACK_LIBRARY)
 
 include (FindPackageHandleStandardArgs)
@@ -17,7 +22,7 @@ find_package_handle_standard_args (SCALAPACK
                                    REQUIRED_VARS SCALAPACK_LIBRARY)
 
 if (SCALAPACK_FOUND)
-    set (SCALAPACK_LIBRARIES ${SCALAPACK_LIBRARY})
+    set (SCALAPACK_LIBRARIES ${SCALAPACK_LIBRARY} ${SCABLACS_LIBRARY})
 
     if (NOT TARGET SCALAPACK::SCALAPACK)
        add_library (SCALAPACK::SCALAPACK UNKNOWN IMPORTED)
