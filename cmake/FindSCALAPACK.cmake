a92f57e4173e9c62d4678f5edf9e3d7299ec2c77
Mark Cianciosa <cianciosamr@ornl.gov>
2021-07-13 16:08:50 -0400

    Add blacs to the link command.

--- cmake/FindSCALAPACK.cmake
+++ cmake/FindSCALAPACK.cmake
@@ -12,7 +12,8 @@ find_library (SCALAPACK_LIBRARY
               DOC "scalapack library")
 
 if (${SCALAPACK_LIBRARY} MATCHES mkl_scalapack_ilp64)
-    find_library (SCABLACS_LIBRARY mkl_blacs_intelmpi_lp64)
+    find_library (BLACS_LIBRARY mkl_blacs_intelmpi_lp64)
+
 endif ()
 
 mark_as_advanced (SCALAPACK_LIBRARY)
@@ -22,11 +23,26 @@ find_package_handle_standard_args (SCALAPACK
                                    REQUIRED_VARS SCALAPACK_LIBRARY)
 
 if (SCALAPACK_FOUND)
-    set (SCALAPACK_LIBRARIES "${SCALAPACK_LIBRARY} ${SCABLACS_LIBRARY}")
+    set (SCALAPACK_LIBRARIES "${SCALAPACK_LIBRARY} ${BLACS_LIBRARY}")
 
-    if (NOT TARGET SCALAPACK::SCALAPACK)
-       add_library (SCALAPACK::SCALAPACK UNKNOWN IMPORTED)
-       set_target_properties (SCALAPACK::SCALAPACK PROPERTIES
+    if (NOT TARGET SCALAPACK::SCALAPACK_BASE)
+       add_library (SCALAPACK::SCALAPACK_BASE UNKNOWN IMPORTED)
+       set_target_properties (SCALAPACK::SCALAPACK_BASE PROPERTIES
                               IMPORTED_LOCATION ${SCALAPACK_LIBRARY})
+    endif ()
+
+    if (SCABLACS_LIBRARY)
+        if (NOT TARGET SCALAPACK::BLACS)
+            add_library (SCALAPACK::BLACS UNKNOWN IMPORTED)
+            set_target_properties (SCALAPACK::BLACS PROPERTIES
+                                   IMPORTED_LOCATION ${BLACS_LIBRARY})
+        endif ()
+    endif ()
+
+    if (NOT TARGET  SCALAPACK::SCALAPACK)
+        add_library (SCALAPACK::SCALAPACK INTERFACE IMPORTED)
+        target_link_libraries (SCALAPACK::SCALAPACK INTERFACE
+                               SCALAPACK::SCALAPACK
+                               $<$<BOOL:BLACS_LIBRARY>SCALAPACK::BLACS>)
     endif ()
 endif ()
