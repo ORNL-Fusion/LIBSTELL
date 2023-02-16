d70cd36f5842486794fd3407b7a9224fe6b3f14c
Mark Cianciosa <cianciosamr@ornl.gov>
2023-02-15 21:04:02 -0500

    Refactor integration path to add an object oriented version.

---
+++ Sources/Modules/integration_path_context.f
@@ -0,0 +1,26 @@
+!*******************************************************************************
+!>  @file integration_path_context.f
+!>  @brief Contains module @ref integration_path_context
+!
+!  Note separating the Doxygen comment block here so detailed decription is
+!  found in the Module not the file.
+!
+!>  Module is part of the LIBSTELL. This modules contains code to define and
+!>  integrate along an arbitray path.
+!*******************************************************************************
+      MODULE integration_path_context
+
+      IMPLICIT NONE
+
+!*******************************************************************************
+!  DERIVED-TYPE DECLARATIONS
+!  1) integration_path_context
+!
+!*******************************************************************************
+!-------------------------------------------------------------------------------
+!>  Context object interface.
+!-------------------------------------------------------------------------------
+      TYPE :: integration_path_context_class
+      END TYPE
+
+      END MODULE
