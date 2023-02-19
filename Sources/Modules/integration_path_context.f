763ecdd3d09d9b8860d59ebde2f641f49ab9ccd8
Mark Cianciosa <cianciosamr@ornl.gov>
2023-02-19 13:45:19 -0500

    Refactor the context objects to contain a run method for the integrand. Refactor search to use the form and add a unit test.

--- Sources/Modules/integration_path_context.f
+++ Sources/Modules/integration_path_context.f
@@ -18,9 +18,55 @@
 !
 !*******************************************************************************
 !-------------------------------------------------------------------------------
-!>  Context object interface.
+!>  Integration context object interface.
 !-------------------------------------------------------------------------------
-      TYPE :: integration_path_context_class
+      TYPE, ABSTRACT :: integration_path_context_class
+      CONTAINS
+         PROCEDURE (integration_function), DEFERRED :: run
       END TYPE
+
+!-------------------------------------------------------------------------------
+!>  Search context object interface.
+!-------------------------------------------------------------------------------
+      TYPE, ABSTRACT :: search_path_context_class
+      CONTAINS
+         PROCEDURE (search_function), DEFERRED :: run
+      END TYPE
+
+!*******************************************************************************
+!  INTERFACE BLOCKS
+!*******************************************************************************
+!-------------------------------------------------------------------------------
+!>  Interface for the integration function.
+!-------------------------------------------------------------------------------
+      INTERFACE
+         FUNCTION integration_function(context, xcart, dxcart,                 &
+     &                                 length, dx)
+         USE stel_kinds
+         IMPORT
+         REAL (rprec)                                       ::                 &
+     &      integration_function
+         CLASS (integration_path_context_class), INTENT(in) :: context
+         REAL (rprec), DIMENSION(3), INTENT(in)             :: xcart
+         REAL (rprec), DIMENSION(3), INTENT(in)             :: dxcart
+         REAL (rprec), INTENT(in)                           :: length
+         REAL (rprec), INTENT(in)                           :: dx
+         END FUNCTION
+      END INTERFACE
+
+!-------------------------------------------------------------------------------
+!>  Interface for the search function.
+!-------------------------------------------------------------------------------
+      INTERFACE
+         FUNCTION search_function(context, xcart1, xcart2)
+         USE stel_kinds
+         IMPORT
+         LOGICAL                                       ::                      &
+     &      search_function
+         CLASS (search_path_context_class), INTENT(in) :: context
+         REAL (rprec), DIMENSION(3), INTENT(in)        :: xcart1
+         REAL (rprec), DIMENSION(3), INTENT(in)        :: xcart2
+         END FUNCTION
+      END INTERFACE
 
       END MODULE
